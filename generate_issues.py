60a53f381708a73237b67736ec33c2e92e156458
TrentonYo <trentonyo@gmail.com>
2024-03-11 17:23:52 -0700

    experiment: testing out apostrophes

--- generate_issues.py
+++ generate_issues.py
@@ -8,8 +8,8 @@ def generate_issue(owner: str, repo: str, title: str, body: str):
             "-H", "Accept: application/vnd.github+json",
             "-H", "X-GitHub-Api-Version: 2022-11-28",
             f"/repos/{owner}/{repo}/issues",
-            "-f", f"title='{title}'",
-            "-f", f"body='{body}'"
+            "-f", f"title={title}",
+            "-f", f"body={body}"
         ]
     )
 
@@ -18,3 +18,4 @@ def generate_issue(owner: str, repo: str, title: str, body: str):
 
 if __name__ == "__main__":
     generate_issue("start-out", "todo-or-not", "python did this", "and I for one am stoked about it")
+    generate_issue("start-out", "todo-or-not", "Let's see about apostrophes", "If you're reading this, bully!")
