c8a1aceea89677a1ed97c13de184c8b41050deb6
TrentonYo <trentonyo@gmail.com>
2024-03-14 16:51:35 -0700

    fix: updated language for when todo-ignore fails

--- todo_or_not/localize.py
+++ todo_or_not/localize.py
@@ -11,7 +11,7 @@ LOCALIZE = {
         "error_cannot_specify_ni_xi": "ERROR: Cannot specify both --ni and --xi",
         "error_is_not_file": "ERROR: Specified path is not a file",
         "error_todo_ignore_not_found": "ERROR: .todo-ignore NOT FOUND! use -i to copy another .ignore OR --force to run without a .todo-ignore (NOT RECOMMENDED)",
-        "error_todo_ignore_not_supported": f"ERROR: .todo-ignore uses unsupported encoding! Supported encodings: {SUPPORTED_ENCODINGS_TODOIGNORE}",
+        "error_todo_ignore_not_supported": f"ERROR: .todo-ignore uses unsupported encoding or doesn't exist! Supported encodings: {SUPPORTED_ENCODINGS_TODOIGNORE}",
         "error_exceeded_maximum_issues": "ERROR: Exceeded maximum number of issues for this run, exiting now",
         "warning_force_overrides_ignore": "WARNING: --force will ignore the contents of the .todo-ignore generated when you specified (.todo-ignore will still be changed, just not used)",
         "warning_run_with_empty_todo_ignore": "WARNING: .todo-ignore was empty (if the file isn't empty, check its encoding), running anyway. To cancel use ",
