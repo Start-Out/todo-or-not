ec664b2833884a1db6f716ac82a307788dfa18b2
TrentonYo <trentonyo@gmail.com>
2024-03-19 22:32:52 -0700

    testing: moved up debug code

--- tests/test_todoon.py
+++ tests/test_todoon.py
@@ -125,8 +125,8 @@ class TestTodoon(unittest.TestCase):
     def test_todoon_takes_individual_targets(self):
         td.main(verbose=True, silent=True, files=self.specific_files_list)
 
-        self.assertEqual(os.environ.get("TODOON_FILES_SCANNED"), '5')
         self.assertEqual(os.getcwd(), 'cheeky')
+        self.assertEqual(os.environ.get("TODOON_FILES_SCANNED"), '5')
 
 
 if __name__ == '__main__':
