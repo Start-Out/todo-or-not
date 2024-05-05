b854647788093dbeede0db1652ee037743cbc2cc
TrentonYo <trentonyo@gmail.com>
2024-05-04 18:20:17 -0700

    fix: set env vars to none for a test (runners have them set automatically)

--- tests/test_github_issues.py
+++ tests/test_github_issues.py
@@ -170,11 +170,19 @@ class TestLiveIssueFeatures(unittest.TestCase):
         self._environment_down()
 
     def test_hit_with_no_env(self):
+        none_env = [
+            ("GITHUB_REPOSITORY", "$NONE"),
+            ("GITHUB_REF_NAME", "$NONE"),
+            ("GITHUB_TRIGGERING_ACTOR", "$NONE")
+        ]
+        self._environment_up(".", env_variables=none_env)
 
         test_hit = todo_or_not.todo_check.Hit('source.txt', 6, ['todo'], ["todo"], 0)
         issue_outcome = test_hit.generate_issue()
 
         assert issue_outcome is False
+
+        self._environment_down()
 
 
 def test_debug_submit_test_issue(example_hit_todo):
