77f85e81974241ebd036ac1bf978c4a0b0baf17e
Trenton Young <trentonyo@gmail.com>
2024-05-27 11:06:15 -0700

    Update version and add new tests for print modes
    This commit updates the version of the app to 0.12.6 in both __init__.py and pyproject.toml file. Additionally, new tests have been added to validate the various print modes including progress bar, summary only, and no output.

--- todo_or_not/__init__.py
+++ todo_or_not/__init__.py
@@ -7,7 +7,7 @@ import datetime
 #
 #####################################################
 
-__version__ = "0.12.5"
+__version__ = "0.12.6"
 iso_string = "2024-05-27"
 
 version_date = datetime.date.fromisoformat(iso_string)
