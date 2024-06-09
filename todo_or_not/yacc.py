17b0057d2236c3463f09a0c7c47da6b704b1a1c5
Trenton Young <trentonyo@gmail.com>
2024-06-09 13:34:56 -0700

    Added accurate parsing for keywords

--- todo_or_not/yacc.py
+++ todo_or_not/yacc.py
@@ -10,12 +10,21 @@ from todo_or_not.todo_check import Hit
 def p_todo_line_with_code(p):
     "todo_line : PRE_LINE_COMMENT todo_line_comment_body"
     reconstructed_line = f"{p[1]} {p[2]["body"]}"
-    p[0] = Hit("file", 1, [p[2]["keyword"]], [reconstructed_line], 0)
+    p[0] = Hit("file", 1, p[2]["keywords"], [reconstructed_line], 0)
 
 
 def p_todo_line_comment_body(p):
     "todo_line_comment_body : TODO_FLAG ELSE"
-    p[0] = {"keyword": p[1].lower(), "body": f"{p[1]}{p[2]}"}
+    find_keywords = p[1].lower().split()
+
+    keywords = []
+
+    if "todo" in find_keywords:
+        keywords.append("todo")
+    if "fixme" in find_keywords:
+        keywords.append("fixme")
+
+    p[0] = {"keywords": keywords, "body": f"{p[1]} {p[2]}"}
 
 
 # Error rule for syntax errors
