a70eba4560a8a45dedce40b3a104ad026ab790f0
TrentonYo <trentonyo@gmail.com>
2024-06-09 15:31:36 -0700

    Add test cases for todo grammar
    This commit introduces unit tests for the todo grammar. These tests cover various scenarios involving simple and complex todo comments, checking for correct parsing and hit generation.

---
+++ tests/test_todo_grammar.py
@@ -0,0 +1,137 @@
+import unittest
+from todo_or_not.todo_grammar import TodoGrammar
+from todo_or_not.todo_check import Hit
+
+
+class TestTodoGrammarPython(unittest.TestCase):
+    def setUp(self):
+        self.grammar = TodoGrammar("py")
+        self.grammar.build()
+
+    def test_simple_line_comment(self):
+        code = "#todo"
+        expected_hit = Hit("file", 1, ["todo"], [code], 0)
+        result = self.grammar.parser.parse(code)
+        assert expected_hit == result
+
+        code = "#fixme"
+        expected_hit = Hit("file", 1, ["fixme"], [code], 0)
+        result = self.grammar.parser.parse(code)
+        assert expected_hit == result
+
+        code = "#fixme todo"
+        expected_hit = Hit("file", 1, ["fixme", "todo"], [code], 0)
+        result = self.grammar.parser.parse(code)
+        assert expected_hit == result
+
+    def test_line_comment_with_leading_comment(self):
+        code = "# a todo"
+        expected_hit = Hit("file", 1, ["todo"], [code], 0)
+        result = self.grammar.parser.parse(code)
+        assert expected_hit == result
+
+        code = "# a fixme"
+        expected_hit = Hit("file", 1, ["fixme"], [code], 0)
+        result = self.grammar.parser.parse(code)
+        assert expected_hit == result
+
+        code = "# a fixme todo"
+        expected_hit = Hit("file", 1, ["fixme", "todo"], [code], 0)
+        result = self.grammar.parser.parse(code)
+        assert expected_hit == result
+
+    def test_line_comment_with_trailing_comment(self):
+        code = "# todo z"
+        expected_hit = Hit("file", 1, ["todo"], [code], 0)
+        result = self.grammar.parser.parse(code)
+        assert expected_hit == result
+
+        code = "# fixme z"
+        expected_hit = Hit("file", 1, ["fixme"], [code], 0)
+        result = self.grammar.parser.parse(code)
+        assert expected_hit == result
+
+        code = "# fixme z todo"
+        expected_hit = Hit("file", 1, ["fixme", "todo"], [code], 0)
+        result = self.grammar.parser.parse(code)
+        assert expected_hit == result
+
+    def test_line_comment_with_leading_and_trailing_comment(self):
+        code = "# a todo z"
+        expected_hit = Hit("file", 1, ["todo"], [code], 0)
+        result = self.grammar.parser.parse(code)
+        assert expected_hit == result
+
+        code = "# a fixme z"
+        expected_hit = Hit("file", 1, ["fixme"], [code], 0)
+        result = self.grammar.parser.parse(code)
+        assert expected_hit == result
+
+        code = "# a fixme z todo"
+        expected_hit = Hit("file", 1, ["fixme", "todo"], [code], 0)
+        result = self.grammar.parser.parse(code)
+        assert expected_hit == result
+
+    def test_leading_code_simple_line_comment(self):
+        code = "123 #todo"
+        expected_hit = Hit("file", 1, ["todo"], [code], 0)
+        result = self.grammar.parser.parse(code)
+        assert expected_hit == result
+
+        code = "123 #fixme"
+        expected_hit = Hit("file", 1, ["fixme"], [code], 0)
+        result = self.grammar.parser.parse(code)
+        assert expected_hit == result
+
+        code = "123 #fixme todo"
+        expected_hit = Hit("file", 1, ["fixme", "todo"], [code], 0)
+        result = self.grammar.parser.parse(code)
+        assert expected_hit == result
+
+    def test_leading_code_line_comment_with_leading_comment(self):
+        code = "123 # a todo"
+        expected_hit = Hit("file", 1, ["todo"], [code], 0)
+        result = self.grammar.parser.parse(code)
+        assert expected_hit == result
+
+        code = "123 # a fixme"
+        expected_hit = Hit("file", 1, ["fixme"], [code], 0)
+        result = self.grammar.parser.parse(code)
+        assert expected_hit == result
+
+        code = "123 # a fixme todo"
+        expected_hit = Hit("file", 1, ["fixme", "todo"], [code], 0)
+        result = self.grammar.parser.parse(code)
+        assert expected_hit == result
+
+    def test_leading_code_line_comment_with_trailing_comment(self):
+        code = "123 # todo z"
+        expected_hit = Hit("file", 1, ["todo"], [code], 0)
+        result = self.grammar.parser.parse(code)
+        assert expected_hit == result
+
+        code = "123 # fixme z"
+        expected_hit = Hit("file", 1, ["fixme"], [code], 0)
+        result = self.grammar.parser.parse(code)
+        assert expected_hit == result
+
+        code = "123 # fixme z todo"
+        expected_hit = Hit("file", 1, ["fixme", "todo"], [code], 0)
+        result = self.grammar.parser.parse(code)
+        assert expected_hit == result
+
+    def test_leading_code_line_comment_with_leading_and_trailing_comment(self):
+        code = "123 # a todo z"
+        expected_hit = Hit("file", 1, ["todo"], [code], 0)
+        result = self.grammar.parser.parse(code)
+        assert expected_hit == result
+
+        code = "123 # a fixme z"
+        expected_hit = Hit("file", 1, ["fixme"], [code], 0)
+        result = self.grammar.parser.parse(code)
+        assert expected_hit == result
+
+        code = "123 # a fixme z todo"
+        expected_hit = Hit("file", 1, ["fixme", "todo"], [code], 0)
+        result = self.grammar.parser.parse(code)
+        assert expected_hit == result
