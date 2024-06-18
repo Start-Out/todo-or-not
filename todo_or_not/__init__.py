a20a829e3b9fc545499bb4d0683182043a7b4712
Trenton Young <trentonyo@gmail.com>
2024-06-18 10:52:27 -0700

    Update version and Github issues workflow in todoon
    This commit accomplishes two main tasks. Firstly, the version number in both the `__init__.py` and `pyproject.toml` files has been updated to 0.13.16. Secondly, the workflow for generating Github issues in `todoon-issues-test.yml` has been altered to run with a GitHub environment by adding the `--github-env` flag.

--- todo_or_not/__init__.py
+++ todo_or_not/__init__.py
@@ -1,5 +1,5 @@
 import datetime
-
+        
 #####################################################
 # VITAL: SEE /scripts/_update_versions.py !!!
 #
@@ -7,7 +7,7 @@ import datetime
 #
 #####################################################
 
-__version__ = "0.13.14"
+__version__ = "0.13.16"
 iso_string = "2024-06-18"
 
 version_date = datetime.date.fromisoformat(iso_string)
